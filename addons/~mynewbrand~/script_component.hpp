c093fadd4ea9996db3a202f0a345210eb8fdcda2
OverlordZorn <dennisAzorn@gmail.com>
2025-02-19 01:35:57 +0100

    re

--- addons/~mynewbrand~/script_component.hpp
+++ addons/~mynewbrand~/script_component.hpp
@@ -1,7 +1,7 @@
-#define COMPONENT Apollo
+#define COMPONENT ~mynewbrand~
 
 // This is being used for the Addon's Name and can be "My Addon Template Framework"
-#define COMPONENT_BEAUTIFIED Apollo Cigarettes
+#define COMPONENT_BEAUTIFIED ~mynewbrand~ Cigarettes
 
 
 #include "\z\cigs\addons\main\script_mod.hpp"
