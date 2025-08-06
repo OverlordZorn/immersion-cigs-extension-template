34d6a2409d3b39c34ab84bc1c74b0a66a3278d45
OverlordZorn <dennisAzorn@gmail.com>
2025-08-06 04:03:58 +0200

    update config to puffs

--- addons/nil/defines.hpp
+++ addons/nil/defines.hpp
@@ -1,14 +1,5 @@
-//// INITSTATETIME defines at which point of the smoking duration which cigarette model should used
-// INITSTATETIME_VARIANT_0 and _1 should not be changed
-#define INITSTATETIME_VARIANT_0 0
-#define INITSTATETIME_VARIANT_1 1
-// INITSTATETIME_VARIANT_2, _3 and _4 should be proportional to MAXTIME
-#define INITSTATETIME_VARIANT_2 66
-#define INITSTATETIME_VARIANT_3 126
-#define INITSTATETIME_VARIANT_4 306
-
-// MAXTIME Defines the duration of the cigarette in seconds
-#define MAXTIME 330
+// TOTALPUFFS Defines how many puffs a cigarette last
+#define TOTALPUFFS 17
 
 // CIGPACK_SIZE Defines how many cigarettes are in the cigarette package.
 #define CIGPACK_SIZE 20
